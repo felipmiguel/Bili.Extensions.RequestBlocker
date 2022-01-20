ad9a5cf7b9e585f6a7ba70e3c289b1d0da3ecf72
Felip Miguel Puig <fmiguel@microsoft.com>
2022-01-20 01:01:54 +0100

    tests working

--- DemoWeb/Program.cs
+++ DemoWeb/Program.cs
@@ -1,42 +1,5 @@
 using Bili.Extensions.RequestBlocker;
 
-//var builder = WebApplication.CreateBuilder(args);
-
-//var services = builder.Services;
-
-//var configuration = builder.Configuration;
-
-//// Add services to the container.
-//services.AddRazorPages();
-//services.AddClientCertificateFilter(configuration, "CertificateFilter");
-
-//builder.WebHost.ConfigureKestrel(options => options.ConfigureHttpsDefaults(options =>
-//      {
-//          options.ClientCertificateMode = Microsoft.AspNetCore.Server.Kestrel.Https.ClientCertificateMode.RequireCertificate;
-
-//      }));
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
-
-//app.UseHttpsRedirection();
-//app.UseStaticFiles();
-
-//app.UseRouting();
-
-//app.UseBlocker();
-
-//app.UseAuthorization();
-
-//app.MapRazorPages();
-
 var builder = WebApplication.CreateBuilder();
 
 var services = builder.Services;
@@ -47,11 +10,6 @@ var configuration = builder.Configuration;
 
 services.AddClientCertificateFilter(configuration, "CertificateFilter");
 
-//builder.WebHost.ConfigureKestrel(options => options.ConfigureHttpsDefaults(options =>
-//{
-//    options.ClientCertificateMode = Microsoft.AspNetCore.Server.Kestrel.Https.ClientCertificateMode.RequireCertificate;
-
-//}));
 builder.WebHost.ConfigureKestrel(options =>
 {
     options.ConfigureHttpsDefaults(httpsOptions =>
@@ -69,8 +27,10 @@ app.UseBlocker();
 
 app.Run(async context =>
 {
-    await context.Response.WriteAsync("Successfuly executed");
     context.Response.StatusCode = 200;
+    await context.Response.StartAsync();
+    await context.Response.WriteAsync("Successfuly executed");
+    
 });
 
 app.Run();
